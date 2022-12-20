d20df28fc6ce19b21dfb2ccfe60a4973376f53ee
Alexandra Rudaya <alexa.rudaya@gmail.com>
2022-12-20 13:17:29 +0300

    Restructure VS solution

---
+++ src/Shop.Infrastructure/LoggerAdapter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Shop.ApplicationCore.Interfaces;
+
+namespace Shop.Infrastructure
+{
+    public sealed class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+
+        public LoggerAdapter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<T>();
+        }
+
+        public void LogError(Exception exception, string? message, params object[] args)
+        {
+            _logger.LogError(exception, message, args);
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+    }
+}
